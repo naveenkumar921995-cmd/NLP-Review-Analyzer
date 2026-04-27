49a4219b5add2d8698dc1a76e7285affe8565e93
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-04-27 23:16:17 +0530

    Update keyword_extractor.py

--- keyword_extractor.py
+++ keyword_extractor.py
@@ -1,8 +1,8 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
-
-def extract_keywords(text):
-    doc = nlp(text)
-    keywords = [token.text for token in doc if token.pos_ in ['NOUN', 'PROPN']]
-    return list(set(keywords))[:10]
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
